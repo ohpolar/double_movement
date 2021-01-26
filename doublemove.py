4bd50a27ee41f04deae41abf58f8c2ec0e079a81
Ben Schuman <ben@macbook-pro.lan>
2021-01-26 18:20:52 -0500

    added main method to doublemovement.py, detects keyboard input for w+a and w+d

--- doublemove.py
+++ doublemove.py
@@ -1,1 +1,16 @@
-import pyvjoy
+import pyvjoy, keyboard
+
+def main():
+    # infinite loop that detects for ('w+a') or ('w+d') in order to apply double movement
+    while True:
+
+        if keyboard.is_pressed('w+a'):
+            move_left()
+        
+        if keyboard.is_pressed('w+d'):
+            move_right()
+
+
+
+if __name__ == '__main__':
+    main()
