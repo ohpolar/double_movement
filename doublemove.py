b8de75f323d522dbe384839d23cf3cc48bcd0ec6
ohpolar <impolar17@gmail.com>
2021-01-27 16:05:00 -0500

    Changed to one file, now uses the batch to install dependencies, and uses vXbox and scpvbus(for the controller driver) to work instead of the xbox360ce program. visit http://vjoystick.sourceforge.net/site/index.php/vxbox to download the drivers.

--- doublemove.py
+++ doublemove.py
@@ -1,44 +1,39 @@
-import sys, subprocess
-
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-# 'pyvjoy'])
-# subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
-# 'keyboard'])
-
-
-import pyvjoy, keyboard
-# import joystick
-
+import pyvjoy, keyboard, pyxinput
 
 print("The script has started without error.")
 
+#pyxinput.test_virtual()
 
-def move_left(vjoy):
-    #print("This will move the joystick to the left.")
-    vjoy.set_axis(pyvjoy.HID_USAGE_X, 0x0)
+def move_left(vjoy, simCon):
+    # print("This will move the joystick to the left.")
+    vjoy.set_axis(pyvjoy.HID_USAGE_X, 0x1)
+    simCon.set_value('AxisLx', 0)
 
 
-def move_right(vjoy):
-    #print("this will move the joystick to the right.")
+def move_right(vjoy, simCon):
+    # print("this will move the joystick to the right.")
     vjoy.set_axis(pyvjoy.HID_USAGE_X, 0x8000)
+    simCon.set_value('AxisLx', 1)
 
-def resetStick(vjoy):
-    #print("The joystick has returned to the neutral position")
+def resetStick(vjoy, simCon):
+    # print("The joystick has returned to the neutral position")
     vjoy.set_axis(pyvjoy.HID_USAGE_X, 0x4000)
+    simCon.set_value('AxisLx', 0)
 
 def main():
     device = pyvjoy.VJoyDevice(1)
+    simCon = pyxinput.vController(1)
     # infinite loop that detects for ('w+a') or ('w+d') in order to apply double movement
     while True:
 
         if keyboard.is_pressed('w+a'):
-            move_left(device)
+            move_left(device, simCon)
         
         elif keyboard.is_pressed('w+d'):
-            move_right(device)
+            move_right(device, simCon)
 
         else:
-            resetStick(device)
+            resetStick(device, simCon)
 
 
 
--- run_double_movement.bat
+++ run_double_movement.bat
@@ -1,5 +1,4 @@
 pip install pyvjoy
 pip install keyboard
-
-
+pip install pyxinput
 python doublemove.py
