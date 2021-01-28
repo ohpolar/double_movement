4c4bea88faa089a122695983e86cfdd394079648
ohpolar <impolar17@gmail.com>
2021-01-27 20:28:20 -0500

    Better double movement, can get larger angles as well. the values are commented in the specific movement functions. The main issue is D+s and D+A are not functioning (diagonal movement backwards. again you NEED to unbind W A S D in fortnite, and lock the input to your mouse under the fortnite sensitivity settings.

--- doublemove.py
+++ doublemove.py
@@ -5,39 +5,57 @@ print("The script has started without error.")
 def move_forward(simCon):
     # Moves joystick forwards
     simCon.set_value('AxisLy', 1)
+    print("forward")
 
 def move_backward(simCon):
     #moves joystick backwards
     simCon.set_value('AxisLy', -1)
+    print("backward")
+
+def move_true_left(simCon):
+    simCon.set_value('AxisLx', -1)
+    print("true_left")
+
+def move_true_right(simCon):
+    simCon.set_value('AxisLx', 1)
+    print("true_right")
 
 def move_left(simCon):
     # Moves joystick to the Left
     simCon.set_value('AxisLx', -32768)
-    simCon.set_value('AxisLy', -8150)
+    simCon.set_value('AxisLy', -10000) # using -8150 here will give you around max angle.
+    print("diagonal left")
 
 def move_right(simCon):
     # Moves joystick to the Right
     simCon.set_value('AxisLx', 1)
-    simCon.set_value('AxisLy', -8150)
+    simCon.set_value('AxisLy', -10000) # using -8150 here will give you around max angle.
+    print("diagonal right")
 
-def move_backLeft(simCon):
+def move_backLeft(simCon): # This is not working right now?
     # moves joystick back and to the left
-    simCon.set_Value('AxisLx', 8000)
-    simCon.set_Value('AxisLy', -1)
+    simCon.set_Value('AxisLx', 1)
+    simCon.set_Value('AxisLy', 1)
+    print("backleft")
 
-def move_backRight(simCon):
+def move_backRight(simCon): # This is not working right now?
     # moves joystick back and to the right
-    simCon.set_Value('AxisLx', 8000)
+    simCon.set_Value('AxisLx', 1)
     simCon.set_Value('AxisLy', -1)
+    print("backright")
 
 def resetStick(simCon):
     # Keeps joystick at the center position for no micro movements in other directions.
     simCon.set_value('AxisLx', 0)
     simCon.set_value('AxisLy', 0)
 
-def resetStickSides(simCon):
+def reset_stick_x(simCon):
     # resets the sticks x coordinates to 0 when walking forwards or backwards
     simCon.set_value('AxisLx', 0)
+
+def reset_stick_y(simCon):
+    # resets the sticks y coordinates to 0 when walking true left or true right
+    simCon.set_value('AxisLy', 0)
 
 def main():
     simCon = pyxinput.vController(1)
@@ -52,11 +70,19 @@ def main():
         
         elif keyboard.is_pressed('w'):
             move_forward(simCon)
-            resetStickSides(simCon)
+            reset_stick_x(simCon)
+
+        elif keyboard.is_pressed('a'):
+            move_true_left(simCon)
+            reset_stick_y(simCon)
         
+        elif keyboard.is_pressed('d'):
+            move_true_right(simCon)
+            reset_stick_y(simCon)
+
         elif keyboard.is_pressed('s'):
             move_backward(simCon)
-            resetStickSides(simCon)
+            reset_stick_x(simCon)
         
         elif keyboard.is_pressed('s+a'):
             move_backLeft(simCon)
